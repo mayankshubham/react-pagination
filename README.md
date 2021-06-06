a3ac3ec35df3adbf221c1df652aa5ec309883407
mayankshubham <mayank.shubham94@gmail.com>
2021-06-06 15:40:26 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-1zaeqk
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-1zaeqk)
---
+++ src/examples/App.js
@@ -0,0 +1,58 @@
+import React, { useState, useMemo } from 'react';
+import Pagination from '../Pagination';
+import data from './data/mock-data.json';
+import './style.scss';
+
+let PageSize = 10;
+
+export default function App() {
+  const [currentPage, setCurrentPage] = useState(1);
+
+  const currentTableData = useMemo(() => {
+    const firstPageIndex = (currentPage - 1) * PageSize;
+    const lastPageIndex = firstPageIndex + PageSize;
+    return data.slice(firstPageIndex, lastPageIndex);
+  }, [currentPage]);
+
+  console.log(
+    currentTableData,
+    data,
+    (currentPage - 1) * PageSize + 1,
+    (currentPage - 1) * (PageSize + 1)
+  );
+  return (
+    <>
+      <table>
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>FIRST NAME</th>
+            <th>LAST NAME</th>
+            <th>EMAIL</th>
+            <th>PHONE</th>
+          </tr>
+        </thead>
+        <tbody>
+          {currentTableData.map(item => {
+            return (
+              <tr>
+                <td>{item.id}</td>
+                <td>{item.first_name}</td>
+                <td>{item.last_name}</td>
+                <td>{item.email}</td>
+                <td>{item.phone}</td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+      <Pagination
+        className="pagination-bar"
+        currentPage={currentPage}
+        totalCount={data.length}
+        pageSize={PageSize}
+        onPageChange={page => setCurrentPage(page)}
+      />
+    </>
+  );
+}
